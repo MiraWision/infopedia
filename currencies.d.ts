6c281e2f82af71313cee70d04384873405096a4e
Yelysei Lukin <yelysei277@gmail.com>
2025-08-08 15:52:34 +0200

    chore: untrack remaining build artifacts (currencies.*, types.*)

--- currencies.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Currency, Currency4217 } from './types';
-import { currencyByCode } from './generated/maps';
-declare const currencies: ReadonlyArray<Currency>;
-declare function getCurrency(code: Currency4217): Currency | undefined;
-export { currencies, currencyByCode, getCurrency };
